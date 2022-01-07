dd995d59c6a4fec8c9a8d42a07148fecc0e757ef
aeskafi <arham.eskafi@gmail.com>
2022-01-07 13:08:09 +0300

    Express Router

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,53 +11,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
-
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-    next();
-})
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you.');
-})
-
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-})
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403; // Forbidden
-    res.end('PUT operation not supported on /dishes');
-})
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes!');
-})
-
-// with params dishId
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send all the details of the dish: ' +
-        req.params.dishId + ' to you!');
-})
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403; // Forbidden
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-})
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-})
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-})
 
 app.use((req, res, next) => {
     res.statusCode = 200;
