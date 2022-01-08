113e958dc755222f75f49b584591c4d774286109
aeskafi <arham.eskafi@gmail.com>
2022-01-08 15:35:44 +0300

    Mongoose Part 2

--- NodeJS/node-mongoose/index.js
+++ NodeJS/node-mongoose/index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Utahppizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
