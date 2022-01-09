a9b438e0766bebd3403281c5199d92e001c1e5cb
aeskafi <arham.eskafi@gmail.com>
2022-01-09 21:00:09 +0300

    Cookies

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -30,31 +30,46 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
 
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-  if (username === 'admin' && password === 'password') {
-    next();
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username === 'admin' && password === 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      next();
+    } else {
+      var err = new Error('You are not authenticated!');
+
+      err.statusCode = 401;
+      return next(err);
+    }
   } else {
-    var err = new Error('You are not authenticated!');
-    err.statusCode = 401;
-    return next(err);
+    if (req.signedCookies.user === 'admin') {
+      next()
+    } else {
+      var err = new Error('You are not authenticated!');
+
+      err.statusCode = 401;
+      return next(err);
+    }
   }
+
 
 }
 
